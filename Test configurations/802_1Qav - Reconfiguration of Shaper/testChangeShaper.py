8118189b9c1e5b7072d0d978804617d5a285d5a1
TausR <Traunh16@student.aau.dk>
2019-12-12 15:29:09 +0100

    Update testChangeShaper.py

--- Test configurations/802_1Qav - Reconfiguration of Shaper/testChangeShaper.py
+++ Test configurations/802_1Qav - Reconfiguration of Shaper/testChangeShaper.py
@@ -2,11 +2,12 @@ import time
 import os
 
 cmdDel = "sudo tc qdisc del dev enp1s0 root"
-cmdSet_P6 = "sudo tc qdisc replace dev enp4s0 parent root handle 100 mqprio num_tc 3 map 2 2 1 0 2 2 2 2 2 2 2 2 2 2 2 2 queues 1@0 1@1 2@2 hw 0"
-cmdSet_P5 = "sudo tc qdisc replace dev enp4s0 parent root handle 100 mqprio num_tc 3 map 2 2 1 0 2 2 2 2 2 2 2 2 2 2 2 2 queues 1@0 1@1 2@2 hw 0"
+cmdSet_P6 = "sudo tc qdisc replace dev enp4s0 parent root handle 100 mqprio num_tc 3 map 0 2 2 2 2 2 1 2 2 2 2 2 2 2 2 2 queues 1@0 1@1 2@2 hw 0"
 cmdSet_Qav1 = "sudo tc qdisc replace dev enp4s0 parent 100:1 cbs idleslope 7808 sendslope -992192 hicredit 12 locredit -97 offload 1"
 cmdSet_Qav2 = "sudo tc qdisc replace dev enp4s0 parent 100:1 cbs idleslope 42762 sendslope -992192 hicredit 12 locredit -97 offload 1"
 
+os.system(cmdSet_p6)
+os.system(cmdSet_Qav2)
 print("Start parameters already set by simple_talker, press (enter) to shaper")
 input(" ")
 
